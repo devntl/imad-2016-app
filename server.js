a8ff5639fc78ef0cccb1d018e9126122dbc797e8
devntl <devrajntl@gmail.com>
2016-09-21 22:20:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articleone', function (req, res) {
+  res.sendFile('Article One Requested and will be send');
+});
+
+app.get('/articletwo', function (req, res) {
+  res.sendFile('Article Two Requested and will be send');
+});
+
+app.get('/articlethree', function (req, res) {
+  res.sendFile('Article Two Requested and will be send');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
